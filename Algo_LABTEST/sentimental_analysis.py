a47c157902148b4e2d554c80da700ef7a85e7fc5
dab07 <harshitsh0711@gmail.com>
2022-11-24 09:35:19 +0530

    Initial commit

---
+++ Algo_LABTEST/sentimental_analysis.py
@@ -0,0 +1,363 @@
+import re
+import pandas as pd
+import re
+import numpy as np
+import pandas as pd
+import transformers as transformers
+from matplotlib import pyplot as plt
+import torch.nn.functional as F
+
+
+data_complaint = pd.read_csv('/Users/hs/PycharmProjects/complaint1700.csv')
+data_complaint['label'] = 0
+data_non_complaint = pd.read_csv('/Users/hs/PycharmProjects/noncomplaint1700.csv')
+data_non_complaint['label'] = 1
+
+data = pd.concat([data_complaint, data_non_complaint], axis=0).reset_index(drop=True)
+
+data.drop(['airline'], inplace=True, axis=1)
+
+from sklearn.model_selection import train_test_split
+
+X = data.tweet.values
+y = data.label.values
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.1, random_state=2020)
+
+test_data = pd.read_csv('/Users/hs/PycharmProjects/test_data.csv')
+test_data = test_data[['id', 'tweet']]
+# print(test_data.sample(5))
+
+import torch
+
+import nltk
+from nltk.corpus import stopwords
+
+
+# nltk.download("stopwords")
+
+def text_preprocessing(s):
+
+    s = s.lower()
+    s = re.sub(r"\'t", " not", s)
+    s = re.sub(r'(@.*?)[\s]', ' ', s)
+    s = re.sub(r'([\'\"\.\(\)\!\?\\\/\,])', r' \1 ', s)
+    s = re.sub(r'[^\w\s\?]', ' ', s)
+    s = re.sub(r'([\;\:\|•«\n])', ' ', s)
+    s = " ".join([word for word in s.split()
+                  if word not in stopwords.words('english')
+                  or word in ['not', 'can']])
+    s = re.sub(r'\s+', ' ', s).strip()
+
+    return s
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+X_train_preprocessed = np.array([text_preprocessing(text) for text in X_train])
+X_val_preprocessed = np.array([text_preprocessing(text) for text in X_val])
+
+tf_idf = TfidfVectorizer(smooth_idf=False)
+X_train_tfidf = tf_idf.fit_transform(X_train_preprocessed)
+X_val_tfidf = tf_idf.transform(X_val_preprocessed)
+
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+
+
+def get_auc_CV(model):
+
+    kf = StratifiedKFold(5, shuffle=True, random_state=1)
+    auc = cross_val_score(
+        model, X_train_tfidf, y_train, scoring="roc_auc", cv=kf)
+
+    return auc.mean()
+
+
+from sklearn.naive_bayes import MultinomialNB
+
+from sklearn.metrics import accuracy_score, roc_curve, auc
+
+
+def evaluate_roc(probs, y_true):
+    preds = probs[:, 1]
+    fpr, tpr, threshold = roc_curve(y_true, preds)
+    roc_auc = auc(fpr, tpr)
+    print(f'AUC: {roc_auc:.4f}')
+
+    y_pred = np.where(preds >= 0.5, 1, 0)
+    accuracy = accuracy_score(y_true, y_pred)
+    print(f'Accuracy: {accuracy * 100:.2f}%')
+
+    # plt.title('Receiver Operating Characteristic')
+    # plt.plot(fpr, tpr, 'b', label='AUC = %0.2f' % roc_auc)
+    # plt.legend(loc='lower right')
+    # plt.plot([0, 1], [0, 1], 'r--')
+    # plt.xlim([0, 1])
+    # plt.ylim([0, 1])
+    # plt.ylabel('True Positive Rate')
+    # plt.xlabel('False Positive Rate')
+    # plt.show()
+
+
+nb_model = MultinomialNB(alpha=1.8)
+nb_model.fit(X_train_tfidf, y_train)
+probs = nb_model.predict_proba(X_val_tfidf)
+
+evaluate_roc(probs, y_val)
+
+def text_preprocessing(text):
+    text = re.sub(r'(@.*?)[\s]', ' ', text)
+    text = re.sub(r'&amp;', '&', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
+
+from transformers import BertTokenizer
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+
+
+def preprocessing_for_bert(data):
+    input_ids = []
+    attention_masks = []
+
+    for sent in data:
+        encoded_sent = tokenizer.encode_plus(
+            text=text_preprocessing(sent),
+            add_special_tokens=True,
+            max_length=MAX_LEN,
+            pad_to_max_length=True,
+            return_attention_mask=True
+        )
+
+        input_ids.append(encoded_sent.get('input_ids'))
+        attention_masks.append(encoded_sent.get('attention_mask'))
+
+    input_ids = torch.tensor(input_ids)
+    attention_masks = torch.tensor(attention_masks)
+
+    return input_ids, attention_masks
+
+all_tweets = np.concatenate([data.tweet.values, test_data.tweet.values])
+encoded_tweets = [tokenizer.encode(sent, add_special_tokens=True) for sent in all_tweets]
+
+max_len = max([len(sent) for sent in encoded_tweets])
+print('Max length: ', max_len)
+
+MAX_LEN = 64
+
+token_ids = list(preprocessing_for_bert([X[0]])[0].squeeze().numpy())
+print('Original: ', X[0])
+print('Token IDs: ', token_ids)
+
+print('Tokenizing data...')
+train_inputs, train_masks = preprocessing_for_bert(X_train)
+val_inputs, val_masks = preprocessing_for_bert(X_val)
+
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
+train_labels = torch.tensor(y_train)
+val_labels = torch.tensor(y_val)
+
+batch_size = 32
+
+train_data = TensorDataset(train_inputs, train_masks, train_labels)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+val_data = TensorDataset(val_inputs, val_masks, val_labels)
+val_sampler = SequentialSampler(val_data)
+val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
+
+import torch
+import torch.nn as nn
+from transformers import BertModel
+
+
+class BertClassifier(nn.Module):
+    def __init__(self, freeze_bert=False):
+        super(BertClassifier, self).__init__()
+        D_in, H, D_out = 768, 50, 2
+
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+
+        self.classifier = nn.Sequential(
+            nn.Linear(D_in, H),
+            nn.ReLU(),
+            # nn.Dropout(0.5),
+            nn.Linear(H, D_out)
+        )
+
+        if freeze_bert:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.bert(input_ids=input_ids,
+                            attention_mask=attention_mask)
+
+        last_hidden_state_cls = outputs[0][:, 0, :]
+        logits = self.classifier(last_hidden_state_cls)
+
+        return logits
+
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+def initialize_model(epochs=4):
+
+    bert_classifier = BertClassifier(freeze_bert=False)
+
+    optimizer = AdamW(bert_classifier.parameters(),
+                      lr=5e-5,
+                      eps=1e-8
+                      )
+
+    total_steps = len(train_dataloader) * epochs
+
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=0, # Default value
+                                                num_training_steps=total_steps)
+    return bert_classifier, optimizer, scheduler
+
+
+import random
+import time
+
+loss_fn = nn.CrossEntropyLoss()
+
+
+def set_seed(seed_value=42):
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
+
+
+def train(model, train_dataloader, val_dataloader=None, epochs=4, evaluation=False):
+    print("Start training...\n")
+    for epoch_i in range(epochs):
+        print(f"{'Epoch':^7} | {'Batch':^7} | {'Train Loss':^12} | {'Val Loss':^10} | {'Val Acc':^9} | {'Elapsed':^9}")
+        print("-" * 70)
+
+        t0_epoch, t0_batch = time.time(), time.time()
+
+        total_loss, batch_loss, batch_counts = 0, 0, 0
+
+        model.train()
+
+        for step, batch in enumerate(train_dataloader):
+            batch_counts += 1
+            b_input_ids, b_attn_mask, b_labels = tuple(t.to() for t in batch)
+
+            model.zero_grad()
+
+            logits = model(b_input_ids, b_attn_mask)
+
+            loss = loss_fn(logits, b_labels)
+            batch_loss += loss.item()
+            total_loss += loss.item()
+
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+            optimizer.step()
+            scheduler.step()
+
+            if (step % 20 == 0 and step != 0) or (step == len(train_dataloader) - 1):
+                time_elapsed = time.time() - t0_batch
+
+                print(
+                    f"{epoch_i + 1:^7} | {step:^7} | {batch_loss / batch_counts:^12.6f} | {'-':^10} | {'-':^9} | {time_elapsed:^9.2f}")
+
+                batch_loss, batch_counts = 0, 0
+                t0_batch = time.time()
+
+        avg_train_loss = total_loss / len(train_dataloader)
+
+        print("-" * 70)
+        if evaluation == True:
+            val_loss, val_accuracy = evaluate(model, val_dataloader)
+            time_elapsed = time.time() - t0_epoch
+
+            print(
+                f"{epoch_i + 1:^7} | {'-':^7} | {avg_train_loss:^12.6f} | {val_loss:^10.6f} | {val_accuracy:^9.2f} | {time_elapsed:^9.2f}")
+            print("-" * 70)
+        print("\n")
+
+    print("Training complete!")
+
+
+def evaluate(model, val_dataloader):
+    model.eval()
+
+    # Tracking variables
+    val_accuracy = []
+    val_loss = []
+
+    for batch in val_dataloader:
+        b_input_ids, b_attn_mask, b_labels = tuple(t.to() for t in batch)
+
+        with torch.no_grad():
+            logits = model(b_input_ids, b_attn_mask)
+
+        loss = loss_fn(logits, b_labels)
+        val_loss.append(loss.item())
+
+        preds = torch.argmax(logits, dim=1).flatten()
+
+        accuracy = (preds == b_labels).cpu().numpy().mean() * 100
+        val_accuracy.append(accuracy)
+
+    val_loss = np.mean(val_loss)
+    val_accuracy = np.mean(val_accuracy)
+
+    return val_loss, val_accuracy
+
+set_seed(42)
+bert_classifier, optimizer, scheduler = initialize_model(epochs=2)
+train(bert_classifier, train_dataloader, val_dataloader, epochs=2, evaluation=True)
+def bert_predict(model, test_dataloader):
+    model.eval()
+
+    all_logits = []
+
+    for batch in test_dataloader:
+        b_input_ids, b_attn_mask = tuple(t.to() for t in batch)[:2]
+
+        with torch.no_grad():
+            logits = model(b_input_ids, b_attn_mask)
+        all_logits.append(logits)
+
+    all_logits = torch.cat(all_logits, dim=0)
+    probs = F.softmax(all_logits, dim=1).cpu().numpy()
+
+    return probs
+
+probs = bert_predict(bert_classifier, val_dataloader)
+evaluate_roc(probs, y_val)
+
+full_train_data = torch.utils.data.ConcatDataset([train_data, val_data])
+full_train_sampler = RandomSampler(full_train_data)
+full_train_dataloader = DataLoader(full_train_data, sampler=full_train_sampler, batch_size=32)
+
+set_seed(42)
+bert_classifier, optimizer, scheduler = initialize_model(epochs=2)
+train(bert_classifier, full_train_dataloader, epochs=2)
+
+print('...Tokenizing data...')
+test_inputs, test_masks = preprocessing_for_bert(test_data.tweet)
+
+test_dataset = TensorDataset(test_inputs, test_masks)
+test_sampler = SequentialSampler(test_dataset)
+test_dataloader = DataLoader(test_dataset, sampler=test_sampler, batch_size=32)
+
+
+probs = bert_predict(bert_classifier, test_dataloader)
+
+threshold = 0.992
+preds = np.where(probs[:, 1] > threshold, 1, 0)
+
+print("Number of tweets predicted non-negative: ", preds.sum())
+
+output = test_data[preds==1]
+list(output.sample(20).tweet)
